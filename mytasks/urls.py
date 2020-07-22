eea368f7dd3e05574c2e010cfda4f82dfd4f8af5
shreyasrami <shreyasrami24.sr@gmail.com>
2020-07-22 20:41:30 +0530

    Add django views to make API call and display data in template for task 3

--- mytasks/urls.py
+++ mytasks/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 
 urlpatterns=[
-   
-    path('<int:num1>/<int:num2>/',views.check,name='check_binary_view')
+    
+    path('',views.weather,name='weather'),
+    path('<int:num1>/<int:num2>/',views.check,name='check_binary_view'),
+    
 ]
--- mytasks/views.py
+++ mytasks/views.py
@@ -1,9 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .binary import check_binary
 from django.http import JsonResponse
+import requests
 # Create your views here.
 
 
-def check(request,num1,num2):
-    response = check_binary(num1,num2)
+def check(request, num1, num2):
+    response = check_binary(num1, num2)
     return JsonResponse(response)
+
+
+def weather(request):
+    if request.method == 'POST':
+        try:
+            city = request.POST['city']
+            url = 'http://api.weatherapi.com/v1/current.json?key=9fafd879443445cc9b5121427200806&q={}'.format(city)
+            data = requests.get(url).json()
+            context = {
+                'City': data['location']['name'],
+                'Temperature': data['current']['temp_c'],
+                'Weather': data['current']['condition']['text'],
+                'Country': data['location']['country'],
+            }
+            
+        except KeyError:
+            context = {
+                'message': 'City not found'
+            }
+        return render(request, 'weather.html', context)
+    else:
+        return render(request,'form.html')
